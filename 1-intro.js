4cd30517a42e7ce07058cc3b0d1c0259f059b580
nstrong <nstrong@spanimation.com>
2021-05-23 17:21:57 -0700

    initial

---
+++ 1-intro.js
@@ -0,0 +1,20 @@
+
+const g = n => n + 1;
+
+const f = n => n * 2;
+
+const wait = time => new Promise(
+    (resolve, reject) => setTimeout(
+        resolve,
+        time
+    ));
+
+wait(300)
+    .then(() => 20)
+    .then(g)
+    .then(f)
+    .then(value => console.log(value)) // 42
+;
+
+
+
